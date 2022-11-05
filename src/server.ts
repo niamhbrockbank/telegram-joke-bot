7df9abaad9d63a303adf30e4360ecc27b8a2f50a
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-05 12:47:54 +0000

    Early stage joke bot

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Telegraf } from 'telegraf'
 import getBotTokenOrQuit from './util/getBotToken';
 
@@ -9,6 +10,23 @@ bot.start((ctx) => ctx.reply("Hello!  Let's talk!"))
 bot.help((ctx) => ctx.reply('Hmm i am not programmed to be helpful, yet!'))
 bot.hears('hello', (ctx) => ctx.reply('Ok, I heard you say hello'))
 bot.command('sing', (ctx) => ctx.reply('La la la!  I got your command.'))
+bot.command('date', (ctx) => ctx.reply(`${new Date}`))
+
+//Reply with a random joke
+bot.command('joke', async (ctx) => {
+    try{
+        const response = await axios.get('https://official-joke-api.appspot.com/jokes/random')
+        const setup = response.data.setup
+        const punchline = response.data.punchline
+        const punchlinePrompts= ['what', 'why', 'how']
+
+        ctx.reply(setup)
+        bot.hears([...punchlinePrompts], (ctx) => ctx.reply(punchline))
+    }
+    catch(error){
+        ctx.reply('Sorry, no can do')
+    }
+    })
 
 bot.launch()
 
